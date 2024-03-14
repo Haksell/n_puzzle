5914dc49924508658149c3f0f8062741bbb30f87
Haksell <axel.brisse@gmail.com>
2024-03-14 22:24:52 +0100

    inefficient permutations.py

---
+++ permutations.py
@@ -0,0 +1,24 @@
+from functools import cache
+
+
+@cache
+def __factorial(n):
+    return 1 if n <= 1 else n * __factorial(n - 1)
+
+
+def perm_to_int(perm):
+    assert sorted(perm) == list(range(len(perm)))
+    return (
+        perm[0] * __factorial(len(perm) - 1)
+        + perm_to_int([x - (x > perm[0]) for x in perm[1:]])
+        if perm
+        else 0
+    )
+
+
+def int_to_perm(n, size):
+    assert isinstance(n, int) and 0 <= n < __factorial(size)
+    if size == 0:
+        return []
+    first, n = divmod(n, __factorial(size - 1))
+    return [first] + [x + (x >= first) for x in int_to_perm(n, size - 1)]
--- test_n_puzzle.py
+++ test_n_puzzle.py
@@ -1,6 +1,8 @@
+import itertools
 import os
 from lib import is_solvable, make_goal
 from parsing import parse
+from permutations import int_to_perm, perm_to_int
 import pytest
 
 DIR_VALID = "puzzles/valid"
@@ -466,3 +468,15 @@ def test_make_goal():
         12,
         11,
     ]
+
+
+def test_perm_to_int():
+    for size in range(6):
+        for n, p in enumerate(itertools.permutations(range(size))):
+            assert perm_to_int(list(p)) == n
+
+
+def test_int_to_perm():
+    for size in range(6):
+        for n, p in enumerate(itertools.permutations(range(size))):
+            assert int_to_perm(n, size) == list(p)
