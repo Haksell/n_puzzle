533c50afef634490c3f0006cf658d8feca384a4b
Axel Brisse <axel.brisse@gmail.com>
2024-04-11 07:23:16 +0200

    noqa

--- src/heuristics/__init__.py
+++ src/heuristics/__init__.py
@@ -1,14 +1,4 @@
-from .conflicts import manhattan_with_conflicts
-from .inversion_distance import inversion_distance
-from .lebesgue import chebyshev, euclidean, manhattan
-from .trivial import constant_zero, hamming
-
-__all__ = [
-    "manhattan_with_conflicts",
-    "inversion_distance",
-    "chebyshev",
-    "euclidean",
-    "manhattan",
-    "constant_zero",
-    "hamming",
-]
+from .conflicts import manhattan_with_conflicts  # noqa
+from .inversion_distance import inversion_distance  # noqa
+from .lebesgue import chebyshev, euclidean, manhattan  # noqa
+from .trivial import constant_zero, hamming  # noqa
