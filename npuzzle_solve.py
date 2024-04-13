3fa0e1572d0ac364c186bfdee5cad59712cc82ee
Axel Brisse <axel.brisse@gmail.com>
2024-04-13 16:50:35 +0200

    gui wip

--- npuzzle_solve.py
+++ npuzzle_solve.py
@@ -2,6 +2,7 @@ import argparse
 from src import heuristics, solvers
 from src.Puzzle import Puzzle
 from src.hash_puzzle import compressed, uncompressed
+from src.solvers import Move
 import time
 import tkinter as tk
 
@@ -39,16 +40,153 @@ def __parse_args():
 
 
 def __launch_gui(puzzle, solution):
+    tiles = list(puzzle)
     root = tk.Tk()
     root.title("n_puzzle")
-    root.geometry("800x600")
+    root.geometry("600x600+100+50")
+    root.resizable(False, False)
+    bd = None
+    playable = False
+
+    top_fr = tk.Frame(root, width=600, height=100, bg="light green")
+    top_fr.pack(fill=tk.X)
+
+    hdg = tk.Label(
+        top_fr,
+        text="  15 PUZZLE GAME  ",
+        font="arial 22 bold",
+        fg="Navy Blue",
+        bg="white",
+    )
+    hdg.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
+
+    play_btn = tk.Button(
+        top_fr,
+        text="New \nGame",
+        bd=5,
+        bg="PaleGreen4",
+        fg="White",
+        font="times 12 bold",
+        # command=new_game,
+    )
+    play_btn.place(relx=0.92, rely=0.5, anchor=tk.E)
+
+    btm_fr = tk.Frame(root, width=600, height=500, bg="light steel blue")
+    btm_fr.pack(fill=tk.X)
+
+    bd_fr = tk.Frame(
+        btm_fr,
+        width=400 + 2,
+        height=400 + 2,
+        relief="solid",
+        bd=1,
+        bg="lemon chiffon",
+    )
+    bd_fr.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+    if playable:
+        btn_state = "normal"
+    else:
+        btn_state = "disable"
+
+    objh = 100
+    objw = 100
+    objx = 0
+    objy = 0
+
+    for r in range(4):
+        for c in range(4):
+            tile = tiles[4 * r + c]
+            if tile == 0:
+                txt = ""
+                bg_color = "White"
+            else:
+                txt = str(tile)
+                bg_color = "RosyBrown1"
+            game_btn = tk.Button(
+                bd_fr,
+                text=txt,
+                relief="solid",
+                bd=1,
+                bg=bg_color,
+                font="times 12 bold",
+                state=btn_state,
+                # command=lambda x=nm: clicked(x),
+            )
+            game_btn.place(x=objx, y=objy, height=objh, width=objw)
+
+            # sq = bd.get_square(r, c)
+            # sq.set_btn(game_btn)
+
+            objx = objx + objw
+        objx = 0
+        objy = objy + objh
+
     root.mainloop()
 
 
 def __main():
     puzzle, hash_pair, heuristic, solver, gui = __parse_args()
     t0 = time.time()
-    solution = solver(puzzle, heuristic, hash_pair)
+    # solution = solver(puzzle, heuristic, hash_pair)
+    solution = [
+        Move.DOWN,
+        Move.LEFT,
+        Move.DOWN,
+        Move.RIGHT,
+        Move.RIGHT,
+        Move.UP,
+        Move.LEFT,
+        Move.DOWN,
+        Move.RIGHT,
+        Move.DOWN,
+        Move.RIGHT,
+        Move.UP,
+        Move.UP,
+        Move.LEFT,
+        Move.DOWN,
+        Move.DOWN,
+        Move.LEFT,
+        Move.LEFT,
+        Move.UP,
+        Move.RIGHT,
+        Move.RIGHT,
+        Move.DOWN,
+        Move.LEFT,
+        Move.LEFT,
+        Move.UP,
+        Move.RIGHT,
+        Move.UP,
+        Move.RIGHT,
+        Move.UP,
+        Move.LEFT,
+        Move.DOWN,
+        Move.LEFT,
+        Move.UP,
+        Move.RIGHT,
+        Move.RIGHT,
+        Move.RIGHT,
+        Move.DOWN,
+        Move.LEFT,
+        Move.LEFT,
+        Move.LEFT,
+        Move.DOWN,
+        Move.RIGHT,
+        Move.RIGHT,
+        Move.RIGHT,
+        Move.UP,
+        Move.LEFT,
+        Move.DOWN,
+        Move.DOWN,
+        Move.RIGHT,
+        Move.UP,
+        Move.LEFT,
+        Move.LEFT,
+        Move.UP,
+        Move.RIGHT,
+    ]
+    print(list(puzzle))
+    print(", ".join("Move." + move.name for move in solution))
     print(
         "".join(move.name[0] for move in solution),
         len(solution),
