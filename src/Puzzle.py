6bc5936012fb1afe5472636267d51146b975fc68
Axel Brisse <axel.brisse@gmail.com>
2024-05-08 19:17:10 +0200

    .

--- src/Puzzle.py
+++ src/Puzzle.py
@@ -31,9 +31,8 @@ class Puzzle:
         return self.__tiles[idx]
 
     def is_correct(self, i):
-        # TODO
+        # TODO: return self[i] == self.__goal[i]
         return __import__("random").choice([False, True])
-        return self[i] == self.__goal[i]
 
     @staticmethod
     def __read_file(filename):
