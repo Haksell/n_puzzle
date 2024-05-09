5c72a1308f8d06e9e72171655ae620a3a2e6d246
Axel Brisse <axel.brisse@gmail.com>
2024-05-09 02:00:43 +0200

    _manhattan_with_conflicts.py

--- src/heuristics/__init__.py
+++ src/heuristics/__init__.py
@@ -1,4 +1,4 @@
-from ._conflicts import manhattan_with_conflicts
+from ._manhattan_with_conflicts import manhattan_with_conflicts
 from ._inversion_distance import inversion_distance
 from ._lebesgue import chebyshev, euclidean, manhattan
 from ._trivial import constant_zero, hamming
